e828102f888b170d7e960fa2890f4d217ad9daf9
CarlinGebyte <91474067+CarlinGebyte@users.noreply.github.com>
2021-11-21 12:32:56 -0500

    Resolviendo problemas con merge

---
+++ src/main/java/com/ciclo4/model/User.java
@@ -0,0 +1,41 @@
+package com.ciclo4.model;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * @author CarlinGebyte
+ */
+@Entity
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+    /**
+     * Atributo ID
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    /**
+     * Atributo Correo
+     */
+    @Column(name = "user_email",length = 50,unique = true, nullable = false)
+    private String email;
+    /**
+     * Atributo Password
+     */
+    @Column(name = "user_password", length = 50, nullable = false)
+    private String password;
+    /**
+     * Atributo UserName
+     */
+    @Column(name = "name", length = 80, nullable = false)
+    private String name;
+}
